d1289f8f6bebd0912cc7107dcc1edf9aa17927ae
Sylwester Lachiewicz <slachiewicz@apache.org>
2023-01-19 22:55:46 +0000

    [MNG-7670] Upgrade Commons Lang from 3.8.1 to 3.12.0 ClassWorlds from 2.6.0 to 2.7.0 Plexus Utils from 2.4.2 to 3.5.0 Hamcrest from 1.3 to 2.2 Mockito from 2.21.0 to 4.11.0 XmlUnit from 1.7.4 to 2.0.9 Guava from 30.1 to 31.1 Maven Plugin Tools Annotations from 3.7.0 to 3.7.1

--- maven-core/src/test/java/org/apache/maven/toolchain/DefaultToolchainManagerTest.java
+++ maven-core/src/test/java/org/apache/maven/toolchain/DefaultToolchainManagerTest.java
@@ -19,7 +19,7 @@
 package org.apache.maven.toolchain;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.isA;
+import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
